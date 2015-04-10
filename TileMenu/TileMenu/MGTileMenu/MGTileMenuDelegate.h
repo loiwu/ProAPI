e5b9a33d4bf7b51669e694e9381aadce7ae65f4e
LoiWu <superloiwu@gmail.com>
2015-04-10 11:59:27 +0800

    TileMenu Build 1.0.10011 - .pch & MGTileMenuDelegate

--- TileMenu/TileMenu/MGTileMenu/MGTileMenuDelegate.h
+++ TileMenu/TileMenu/MGTileMenu/MGTileMenuDelegate.h
@@ -9,3 +9,10 @@
 #ifndef TileMenu_MGTileMenuDelegate_h
 #define TileMenu_MGTileMenuDelegate_h
 #endif
+
+#define MG_PAGE_SWITCHING_TILE_INDEX -1
+
+@class MGTileMenuController;
+@protocol MGTileMenuDelegate <NSObject>
+
+@end
